3bfd5bdb6166638ddf6d8bd82a0136048fec481e
Anh <ngocnanh240220012gmail.com>
2022-06-29 17:05:43 +0700

    full main

--- js/index.js
+++ js/index.js
@@ -7,3 +7,29 @@ $(document).ready(function() {
     });
     $("body").addClass("abc");
 });
+
+// Add active navbar
+var header = document.getElementById("headerEgitech");
+var li = header.getElementsByClassName("homepage", "aboutUs", "services", "solution", "blog", "carrer", "contact");
+for (var i = 0; i < li.length; i++) {
+    li[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        if (current.length > 0) {
+            current[0].className = current[0].className.replace(" active", "");
+        }
+        this.className += " active";
+    });
+}
+
+// menu burger
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
