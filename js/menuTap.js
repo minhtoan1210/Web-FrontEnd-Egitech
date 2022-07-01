2d9c726532ee22a2a57a0ffaf8ad01fb22864f7b
Anh <ngocnanh240220012gmail.com>
2022-07-01 16:46:22 +0700

    1/7 16:46

--- js/menuTap.js
+++ js/menuTap.js
@@ -1,4 +1,5 @@
 function openCity(evt, cityName) {
+    debugger;
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
