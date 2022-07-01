3586e3dad4965cc8994ea323f7654ec171508d3b
Anh <ngocnanh240220012gmail.com>
2022-07-01 14:40:32 +0700

    Anh

---
+++ js/blog.js
@@ -0,0 +1,6 @@
+// var a = document.querySelectorAll("ul.nav-menu li.nav-item a");
+// console.log(a);
+// a.forEach(element => {
+//     console.log(element.getAttribute("class"));
+//     if(element.getAttribute("class")lastIndexOf(searchElement,indexNumber);)
+// });
