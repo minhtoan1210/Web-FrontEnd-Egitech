9f223eab4a3886b8ab009b3ac09d5a8d17cf62f9
Anh <ngocnanh240220012gmail.com>
2022-07-02 23:47:33 +0700

    commit

--- js/header.js
+++ js/header.js
@@ -3,11 +3,11 @@
 //     console.log("abc")
 // })
 
-const currentLocation = location.href;
-const menuItem = document.querySelectorAll('a');
-const menuLength = menuItem.length
-for (let i = 0; i < menuLength; i++) {
-    if (menuItem[i].href === currentLocation) {
-        menuItem[i].className === 'active'
+// const currentLocation = location.href;
+const menuItem = document.querySelectorAll('a.nav-link');
+// const menuLength = menuItem.length
+for (let i = 0; i < menuItem.length; i++) {
+    if (menuItem[i].getAttribute("href") === location.pathname) {
+        menuItem.classList.add("active");
     }
 }
