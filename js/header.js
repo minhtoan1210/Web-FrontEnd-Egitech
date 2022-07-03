fb9050e41e7135aa45f6278a644dea7d51e8f80c
Anh <ngocnanh240220012gmail.com>
2022-07-03 13:33:42 +0700

    13:33 03/07

--- js/header.js
+++ js/header.js
@@ -1,13 +1,6 @@
-// $(document).on('click', 'ul li', function() {
-//     // $(this).addClass('active').siblings().removeClass('active')
-//     console.log("abc")
-// })
-
-// const currentLocation = location.href;
-const menuItem = document.querySelectorAll('a.nav-link');
-// const menuLength = menuItem.length
-for (let i = 0; i < menuItem.length; i++) {
+var menuItem = document.querySelectorAll('a.nav-link');
+for (var i = 0; i < menuItem.length; i++) {
     if (menuItem[i].getAttribute("href") === location.pathname) {
-        menuItem.classList.add("active");
+        menuItem[i].classList.add("active");
     }
 }
--- js/menuTap.js
+++ js/menuTap.js
@@ -1,4 +1,4 @@
-function openCity(evt, cityName) {
+function openMenu(evt, tapName) {
     // debugger;
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -9,6 +9,6 @@ function openCity(evt, cityName) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(tapName).style.display = "block";
     evt.currentTarget.className += " active";
 }
