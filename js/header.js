75a79cd4c01c2a680a28bdecef697b1cba928487
Anh <ngocnanh240220012gmail.com>
2022-07-04 14:24:47 +0700

    done 14:24

--- js/header.js
+++ js/header.js
@@ -3,8 +3,7 @@ for (var i = 0; i < menuItem.length; i++) {
     console.log(menuItem[i].getAttribute("href"));
     console.log(location.pathname);
     if (menuItem[i].getAttribute("href") === location.pathname) {
-        console.log("ok")
-        menuItem[i].classList.add("active");
+
+        menuItem[i].classList.add("navActive");
     }
 }
-console.log("sadasd");
--- js/index.js
+++ js/index.js
@@ -8,18 +8,6 @@ $(document).ready(function() {
     $("body").addClass("abc");
 });
 
-// // Add active navbar
-// var header = document.getElementById("navbarEgitech");
-// var li = header.getElementsByClassName("homepage", "aboutUs", "services", "solution", "blog", "carrer", "contact");
-// for (var i = 0; i < li.length; i++) {
-//     li[i].addEventListener("click", function() {
-//         var current = document.getElementsByClassName("active");
-//         if (current.length > 0) {
-//             current[0].className = current[0].className.replace("active", "");
-//         }
-//         this.className += "active";
-//     });
-// }
 
 // menu burger
 const menuBtn = document.querySelector('.menu-btn');
