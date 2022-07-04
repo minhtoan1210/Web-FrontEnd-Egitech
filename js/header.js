26894ca95f77c9f13bdf56cf19752743a7841f55
Anh <ngocnanh240220012gmail.com>
2022-07-04 14:00:52 +0700

    done

--- js/header.js
+++ js/header.js
@@ -1,6 +1,10 @@
 var menuItem = document.querySelectorAll('a.nav-link');
 for (var i = 0; i < menuItem.length; i++) {
+    console.log(menuItem[i].getAttribute("href"));
+    console.log(location.pathname);
     if (menuItem[i].getAttribute("href") === location.pathname) {
+        console.log("ok")
         menuItem[i].classList.add("active");
     }
 }
+console.log("sadasd");
